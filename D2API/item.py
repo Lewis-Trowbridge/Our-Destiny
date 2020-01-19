4dd1b1cb169e8c1b1cfdffdb6aa6be685dbc8c5c
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2020-01-19 15:29:45 +0000

    Removed the separate class and merged the method to get instanced stats into the main item class, which when called will modify the stats of the already existing object - this was done to reduce the amount of API requests being sent, which was the main issue with my previous ideal approach.

--- D2API/item.py
+++ D2API/item.py
@@ -2,17 +2,17 @@
 
 class d2item():
 
-
     def __init__(self, item_request_json, d2characterobject):
+        self.instanced_item = False
         self.stats = {}
         self.owner_object = d2characterobject
         item_data_json = self.owner_object.client_object.GetFromDB(item_request_json["itemHash"], "InventoryItem")
         self.description = item_data_json["displayProperties"]["description"]
         self.name = item_data_json["displayProperties"]["name"]
         if item_data_json["displayProperties"]["hasIcon"]:
-            self.icon_url = item_data_json["displayProperties"]["icon"]
+            self.icon_url = "https://www.bungie.net" + item_data_json["displayProperties"]["icon"]
         try:
-            self.screenshot_url = item_data_json["screenshot"]
+            self.screenshot_url = "https://bungie.net" + item_data_json["screenshot"]
         except KeyError:
             self.screenshot_url = None
         try:
@@ -20,20 +20,20 @@ class d2item():
         except KeyError:
             self.lore = None
         try:
+            self.instance_id = item_request_json["itemInstanceId"]
+        except KeyError:
+            self.instance_id = None
+        try:
             for stat_hash in self.owner_object.client_object.GetFromDB(item_data_json["stats"]["statGroupHash"], "StatGroup")["scaledStats"]:
                 self.stats[self.owner_object.client_object.GetFromDB(stat_hash["statHash"], "Stat")["displayProperties"]["name"]] = item_data_json["stats"]["stats"][str(stat_hash["statHash"])]["value"]
         except KeyError:
             self.stats = None
 
-
-
-class d2instancedItem(d2item):
-
-
-    def __init__(self, item_request_json, d2characterobject):
-        super().__init__(item_request_json, d2characterobject)
-        self.instance_id = item_request_json["itemInstanceId"]
-        item_instance_json = self.owner_object.client_object.GetInstancedItem(self.owner_object.membership_type, self.instance_id)
-        stat_hashes = item_instance_json["stats"]["data"]["stats"].keys()
-        for stat_hash in stat_hashes:
-            self.stats[self.owner_object.client_object.GetFromDB(stat_hash, "Stat")["displayProperties"]["name"]] = item_instance_json["stats"]["data"]["stats"][stat_hash]["value"]
+    def BecomeInstanced(self):
+        if self.instance_id is not None:
+            item_instance_json = self.owner_object.client_object.GetInstancedItem(self.owner_object.membership_type, self.instance_id)
+            stat_hashes = item_instance_json["stats"]["data"]["stats"].keys()
+            for stat_hash in stat_hashes:
+                self.stats[self.owner_object.client_object.GetFromDB(stat_hash, "Stat")["displayProperties"]["name"]] = item_instance_json["stats"]["data"]["stats"][stat_hash]["value"]
+        else:
+            raise Exception("Item does not have an instance ID")
